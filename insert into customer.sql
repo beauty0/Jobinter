0e61189b04d813c2b143aabc7a1db701ef33abbb
Isibeal <isigirluk@yahoo.co.uk>
2018-03-11 08:43:56 +0000

    insert into customer

---
+++ insert into customer.sql
@@ -0,0 +1,57 @@
+------insert values into Customer table------
+
+SET IDENTITY_INSERT Customers ON;
+
+ INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(2,'Cinda','WHITMER','Cinda@gmail.com',23);
+
+ INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(3,'Yamine','Adam','Yamad@gmail.com',45);
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(4,'Anthony','Scot','Thony@gmail.com',25);
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email, Age)
+VALUES(5,'Helen','Mayor','Helen@gmail.com',30);
+
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(6,'ELENA','ALVO','Elena@gmail.com',28);
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(7,'RLFINA','FANU','Rlfinad@gmail.com',28);
+
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(8,'Mary','Esat','May@gmail.com',24);
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email, Age)
+VALUES(9,'Mina','Doyle','Mina@gmail.com',32);
+
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(10,'Dunne','Molony','Dunne@gmail.com',20);
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(11,'Casey','Hayes','Casy@gmail.com',36);
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(12,'Foley','Maguire','fole@gmail.com',29);
+
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(13,'Peter','Clarke','peter@gmail.com',50);
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email, Age)
+VALUES(14,'Orla','Stewart','orlat@gmail.com',35);
+
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(15,'Kane','Meagher','Kane@gmail.com',24);
+
+INSERT into dbo. Customers(CUSTOMER_ID,First_NAME,Last_name,Email,Age)
+VALUES(16,'Foley','MacCrait','Foky@gmail.com',46);
+
+
+
+SET IDENTITY_INSERT  Customers OFF;
