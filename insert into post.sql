45a97db02753c5115d432290177f766627d5f128
Isibeal <isigirluk@yahoo.co.uk>
2018-03-14 04:37:41 +0000

    done

--- insert into post.sql
+++ insert into post.sql
@@ -1,32 +1,38 @@
 SET IDENTITY_INSERT dbo.Post ON;
  INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(51, 2,'Can you guess my name','2018-03-14 13:23:44');
+VALUES(52, 92,'Can you guess my name','2018-03-14 13:23:44');
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(52, 3,'Am having a party','2018-02-14 11:13:44');
+VALUES(53, 93,'Am having a party','2018-02-14 11:13:44');
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(53, 4,'l am just loving it','2017-05-14 13:03:14' );
+VALUES(54, 94,'l am just loving it','2017-05-14 13:03:14' );
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(54, 5,'What is going on here','2018-01-21  12:03:14' );
+VALUES(55, 95,'What is going on here','2018-01-21  12:03:14' );
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(55, 6,'Am off work today', '2018-01-12 15:03:14');
+VALUES(56, 96,'Am off work today', '2018-01-12 15:03:14');
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(56, 8,'l am justing having a bad day','2017-10-25 17:06:14' );
+VALUES(57, 97,'is a bad day','2017-10-25 17:06:14' );
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(57, 9,'l like this site','2018-01-25  14:06:14 ');
+VALUES(58, 98,'l like this site','2018-01-25  14:06:14 ');
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(58, 10,'What is happening here', '2018-02-15 15:06:14' );
+VALUES(59, 99,'What is happening here', '2018-02-15 15:06:14' );
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(59, 11,'Hello everyone','2018-01-15 16:06:14' );
+VALUES(60, 100,'Hello everyone','2018-02-15 16:06:14' );
 
 INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
-VALUES(60, 12, 'Is a good day', '2018-01-15 16:06:14');
+VALUES(61, 101, 'Hello there', '2018-01-15 16:06:14');
+
+INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
+VALUES(62, 102, 'What are you doing', '2018-01-15 16:06:14');
+
+INSERT into dbo.Post(Post_ID,User_ID,Post_Message,Post_time)
+VALUES(63, 103, 'Is a good day', '2018-01-15 16:06:14');
 
 SET IDENTITY_INSERT dbo.Post OFF;
