46beeca263f6026cb7bfb7e7bf8f4bfd256093cf
Isibeal <isigirluk@yahoo.co.uk>
2018-03-14 04:22:28 +0000

    autocrement

--- create chat table.sql
+++ create chat table.sql
@@ -3,7 +3,7 @@ Go
 
 
 CREATE TABLE users(
-user_ID INT IDENTITY  NOT NULL PRIMARY KEY,
+User_ID int IDENTITY(91,1)  not null PRIMARY KEY,
 user_NAME VARCHAR (50) NOT NULL,
 user_age int  NOT NULL,
 Join_date  Date  Not null,
@@ -12,7 +12,7 @@ Location Varchar  (100)
 );
 
 CREATE Table Post (
-Post_ID INT IDENTITY NOT NULL PRIMARY KEY,
+Post_ID INT IDENTITY(51,1) NOT NULL PRIMARY KEY,
 User_ID INT NOT NULL,
 Post_Message Varchar(MAX),
 Post_time  Datetime not null,
