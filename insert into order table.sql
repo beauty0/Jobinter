e4705fd39c9d119af337615f8f91c793e88fc5b8
Isibeal <isigirluk@yahoo.co.uk>
2018-03-15 21:02:17 +0000

    order updated

--- insert into order table.sql
+++ insert into order table.sql
@@ -3,51 +3,51 @@
 
 SET IDENTITY_INSERT Orders ON;
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES(2,61,41, 21,16.09,5,111);
+INSERT into dbo.Orders(order_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(111,2,61,41, 21,16.09,5);
 
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES(3,62,42, 22,4.13,2,112);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(112,3,62,42, 22,4.13,2);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES(4,63,43, 23,20.00, 6, 113);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(113,4,63,43, 23,20.00, 6);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 5, 64, 44, 24, 15.40, 3, 114);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(114, 5, 64, 44, 24, 15.40, 3);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 6, 65, 45, 25, 11.30, 6, 115);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(115, 6, 65, 45, 25, 11.30, 6);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 7, 66, 46, 26, 16.00, 3, 116);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(116, 7, 66, 46, 26, 16.00, 3);
 
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES(8, 67, 47, 27, 4.50, 8,117);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(117,8, 67, 47, 27, 4.50, 8);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount, Order_ID)
-VALUES(9, 68,48, 28, 10.50, 7, 118);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(118,9, 68,48, 28, 10.50, 7);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES(10, 69, 49, 29, 3.50, 4, 119);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(119,10, 69, 49, 29, 3.50, 4);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 11, 70, 50, 30, 50.45, 1, 120);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(120 11, 70, 50, 30, 50.45, 1);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 12, 71, 51, 31, 30.99, 1, 121);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES( 121, 12, 71, 51, 31, 30.99, 1);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 13, 72,52, 32, 40.99, 3,122);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES( 122,13, 72,52, 32, 40.99, 3);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 14, 73,53, 33, 15.99, 2, 123);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(123, 14, 73,53, 33, 15.99, 2);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 15, 74, 54, 34, 12.99, 5, 124);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES(124, 15, 74, 54, 34, 12.99, 5);
 
-INSERT into dbo.Orders(Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount,Order_ID)
-VALUES( 16, 75, 55, 35, 30.99, 4,125);
+INSERT into dbo.Orders(order_id,Customer_ID, Address_ID,Product_ID,timeID,OrderPrice,OrderAmount)
+VALUES( 125, 75, 55, 35, 30.99, 4);
 
 SET IDENTITY_INSERT Orders Off;
